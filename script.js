17077ac735abf9e68e118940bc140ad8ba6b1e1a
Glitch (jemily-wedding-photobooth) <none>
2018-08-17 03:34:51 +0000

    😇🏄 Checkpoint ./script.js:514275/156:1632987/16 ./index.html:514275/533

--- script.js
+++ script.js
@@ -10,24 +10,19 @@ console.log('hiiii');
       context = canvas.getContext('2d');
       
       tracking.ColorTracker.registerColor('custom', function(r, g, b) {
-        return r >= 210 && g >= 210 && b >= 210;
+        return r >= 240 && g >= 240 && b >= 240;
       });
       
-      var tracker = new tracking.ColorTracker(["cyan", "magenta"]);
+      var tracker = new tracking.ColorTracker(["cyan", "magenta", "custom"]);
       tracking.track('#video', tracker, { camera: true });
       testing = false;
       tracker.on('track', function(event) {
         context.clearRect(0, 0, canvas.width, canvas.height);
         event.data.forEach(function(rect) {
-          if (!testing) {
-            console.log("hello");
-            testing = true;
-          }
-          console.log(rect)
-          context.strokeStyle = "#000";
+          context.strokeStyle = "#00ff00";
           context.strokeRect(rect.x, rect.y, rect.width, rect.height);
           context.font = '11px Helvetica';
-          context.fillStyle = "#000";
+          context.fillStyle = "#00ff00";
           context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
           context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
         });
