cc9e37d811745ce3770e8ff7c540ab777520f7c6
Glitch (jemily-wedding-photobooth) <none>
2018-08-20 00:19:13 +0000

    ✌️👨‍❤️‍👨 Checkpoint ./cbuffer.js:1633442/9797 ./script.js:1633442/172:1633460/120 ./index.html:1633442/52

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var capture;
 var tracker;
 
+const historySize = 50;
 
 trackerHistory = {
-  "cyan": [],
-  "white": [],
+  "cyan": new CBuffer(historySize),
+  "white": new CBuffer(historySize),
 }
 
 function setup() {
@@ -26,7 +27,6 @@ function setup() {
     tracking.ColorTracker.registerColor('white', function(r, g, b) {
      return r >= 230 && g >= 230 && b >= 230;
     });
-    //tracking.Image.blur(pixels, width, height, 30);
   
     var tracker = new tracking.ColorTracker(Object.keys(trackerHistory));
 
@@ -43,13 +43,15 @@ toDraw = [];
 
 function onTrack(event) {
   event.data.forEach(function (r) {
-    trackerHistory[r.color].push
+
+    trackerHistory[r.color].push(r);
+
     toDraw.push(new Flower(r));
   });
 }
 
 function draw() {
-  now = Date.now()
+  var now = Date.now()
   clear();
   nextToDraw = [];
   toDraw.forEach((thing) => {
