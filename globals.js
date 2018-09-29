17dcece11702ee72ea62aefc38814c2bec75e4f1
Glitch (jemily-wedding-photobooth) <none>
2018-09-29 03:15:00 +0000

    🍘💖 Checkpoint ./style.css:1633442/170 ./script.js:1633442/195 ./globals.js:1633442/12

--- globals.js
+++ globals.js
@@ -1,8 +1,8 @@
 viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-canvasWidth = viewportWidth / 2;
-canvasHeight = viewportHeight / 2;
+canvasWidth = viewportWidth/2;
+canvasHeight = viewportHeight/2;
 
 RED = 0;
 GREEN = 1;
