da541e4d234b231b4b233c83585d71469f07ea3d
Glitch (jemily-wedding-photobooth) <none>
2018-09-30 15:39:53 +0000

    🍽😇 Checkpoint ./ui.js:1633442/1723

--- ui.js
+++ ui.js
@@ -3,6 +3,17 @@
 
 // I don't even know why I'm trying to encapsulate this in a class, it's a pretty futile gesture at this point
 
+  
+function createDOMElement(s) {
+  let el = document.createElement('div');
+  el.innerHTML = s;
+  return el.firstChild;
+}
+
+function destroyElement(elem) {
+   elem.parentNode.removeChild(elem);  
+}
+
 class UI {
 
   constructor () {
@@ -18,19 +29,19 @@ class UI {
   }
 
   drawInitial() {
-    let title = document.createElement('div');
-    let start = document.createElement('div');
-    title.innerHTML = "Leave a video message for John and Emily!<br/>(use the lightbulbs to draw in the air)";
-    start.innerHTML = "Press ENTER to start";
-    title.className ="initial title glow";
-    start.className = "initial start glow"
+    let title = createDOMElement(`<div class="initial title glow">
+       Leave a video message for John and Emily!<br/>(use the lightbulbs to draw in the air)
+    </div>`);
+    let start = createDOMElement(`<div class="initial start glow">
+       Press ENTER to start
+    </div>`);    
     document.body.appendChild(title);
     document.body.appendChild(start);
   }
   
   destroyInitial() {
     for (let elem of document.querySelectorAll(".initial")) {
-      elem.parentNode.removeChild(elem);
+      ;
     }
   }
   
