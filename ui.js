f075b9de01753ed6e25c6a66201a6f3506589b1e
Glitch (jemily-wedding-photobooth) <none>
2018-09-30 14:27:47 +0000

    🏵🦄 Checkpoint ./styles/shine.css:1633442/3711 ./ui.js:1633442/62 ./styles/glow.css:1633442/306 ./styles/style.css:1633442/9

--- ui.js
+++ ui.js
@@ -6,8 +6,8 @@ function drawInitialStateUI() {
   let start = document.createElement('div');
   title.innerHTML = "Leave a magical message for John and Emily!";
   start.innerHTML = "Press SPACE to start";
-  title.className ="initial title confirm_selection";
-  start.className = "initial start confirm_selection"
+  title.className ="initial title glow";
+  start.className = "initial start glow"
   document.body.appendChild(title);
   document.body.appendChild(start);
 }
